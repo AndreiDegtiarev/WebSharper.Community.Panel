415792d4c7554e696dd49097843e252670dbbed4
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2018-04-05 22:35:44 +0200

    update package info

--- tools/settings.fsx
+++ tools/settings.fsx
@@ -3,6 +3,7 @@ module Settings
     #load @"..\..\WebSharper.Community.Build\gloabalSettings.fsx"
     let projectName = "Panel"
     let projectDescription = "WebSharper floating panel component"
-    let dependencies = [("WebSharper.UI.Next","")
+    let dependencies = [("WebSharper.UI","")
+                        ("WebSharper.FSharp","")
                         ("WebSharper.Community.PropertyGrid",globalSettings.version)]
     let releaseNotes = ""
